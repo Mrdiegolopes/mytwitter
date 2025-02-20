bc8fab0aac4b266cca006f77c229ec4a18d3e97e
Francisco Arley Coelho Barros <107206805+Ch1c40@users.noreply.github.com>
2025-02-20 23:19:55 +0000

    Commit da atualização da classe RepositorioUsuario (20/02) sujeito a alterações

--- src/RepositorioUsuarios.py
+++ src/RepositorioUsuarios.py
@@ -1,5 +1,31 @@
+import perfil
+
+class UJCExpetion(Exception):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+class UNCExpetion(Exception):
+    def __init__(self, *args):
+        super().__init__(*args)
+
 class RepositorioUsuarios():
     def __init__(self):
         self.__perfis = []
 
+    def cadastrar(self, usuario):
+        if self.buscar(usuario) is not None:
+            raise UJCExpetion(f"Usuario {perfil.usuario} já está cadastrado!")
+        self.__perfis.append(usuario)
+         
+    def buscar(self, name):
+        for usuario in self.__perfis:
+            if perfil.usuario == name:
+                return usuario
+        return None
     
+    def atualizar(self, usuario):
+        for i, u in enumerate(self.__perfis):
+            if u.usuario == perfil.usuario:
+                self.__perfis[i] == usuario
+                return
+        raise UNCExpetion(f"Usuario {perfil.usuario} não está cadastrado")
