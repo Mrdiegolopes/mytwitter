8e9cafd481a525eeae5d05a0c26634defd3017fa
Diego Lopes <dieolpes77@gmail.com>
2025-02-23 02:10:39 -0300

    adc cod em execeoçes

---
+++ src/excecoes.py
@@ -0,0 +1,6 @@
+# Exceções personalizadas
+class UJCException(Exception): pass
+class UNCException(Exception): pass
+class PIException(Exception): pass
+class PDException(Exception): pass
+class MFPException(Exception): pass
