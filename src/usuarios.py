694ad41a0020cc299a2d7b09b752b9374afe7797
Diego Lopes <129468376+Mrdiegolopes@users.noreply.github.com>
2025-02-25 13:12:32 -0300

    Update usuarios.py

--- src/usuarios.py
+++ src/usuarios.py
@@ -1,24 +1,17 @@
-
-from mytwitter import Perfil
+from perfil import Perfil
 
 class PessoaFisica(Perfil):
-    
     def __init__(self, usuario, cpf):
-       
         super().__init__(usuario)
         self.__cpf = cpf
-    
+
     def get_cpf(self):
-        
         return self.__cpf
 
 class PessoaJuridica(Perfil):
-    
     def __init__(self, usuario, cnpj):
-      
         super().__init__(usuario)
         self.__cnpj = cnpj
-    
+
     def get_cnpj(self):
-        
         return self.__cnpj
